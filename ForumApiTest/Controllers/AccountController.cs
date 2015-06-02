94de752663cc03fe761a74ba6359ff131d045602
louis1901 <louis831@hotmail.com>
2015-06-02 22:20:46 +0800

    add logout

--- ForumApiTest/Controllers/AccountController.cs
+++ ForumApiTest/Controllers/AccountController.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using System.Threading.Tasks;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security;
 
 
 namespace ForumApiTest.Controllers
@@ -51,6 +53,19 @@ namespace ForumApiTest.Controllers
             }
 
             return Ok();
+        }
+
+        // POST api/Account/Logout
+        [Route("Logout")]
+        public IHttpActionResult Logout()
+        {
+            Authentication.SignOut(CookieAuthenticationDefaults.AuthenticationType);
+            return Ok();
+        }
+
+        private IAuthenticationManager Authentication
+        {
+            get { return Request.GetOwinContext().Authentication; }
         }
 
         private IHttpActionResult GetErrorResult(IdentityResult result)
